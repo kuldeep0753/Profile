550ad000875aadfcecc38b3d36676b6e2807f01c
Kuldeep Singh <v-kukushwaha@microsoft.com>
2024-11-29 15:28:48 +0530

    hamburger setup

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -23,20 +23,31 @@ toggleDay.addEventListener("click", function () {
 
 let hamburger = "assets/images/hamburger.svg";
 let navBar = document.querySelector(".navbar");
-window.addEventListener("resize", function () {
-  console.log("trigger");
+let hideNav = document.querySelector(".nav-item");
+
+window.addEventListener("resize", addHamburger);
+
+function addHamburger() {
+  let existingHamburger = document.getElementById("hamburger-icon");
+  if (window.innerWidth < 600) {
+    if (!existingHamburger) {
+      let imgEle = document.createElement("img");
+      //   console.log(typeof(imgEle))
+      imgEle.src = hamburger;
+      imgEle.id = "hamburger-icon";
+      navBar.appendChild(imgEle);
+      console.log("trigger");
+    }
+  } else {
+    if (existingHamburger) {
+      existingHamburger.remove();
+    }
+  }
+  // show/hide element
   let hideNav = document.querySelector(".nav-item");
   if (window.innerWidth < 600) {
     hideNav.style.display = "none";
   } else {
     hideNav.style.display = "block";
   }
-});
-if (window.innerWidth < 600) {
-  function addHamburger() {
-    let imgEle = document.createElement("img");
-    imgEle.src = hamburger;
-    navBar.appendChild(imgEle);
-  }
-  addHamburger()
 }
